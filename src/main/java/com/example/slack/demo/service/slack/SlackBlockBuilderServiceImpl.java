894eb7fb501bb2856f56de9a6bcd7cd59261b14e
Alexandru Oliva <aoliva@griddynamics.com>
2024-11-23 17:27:06 +0200

    Fixed mail address placeholder

--- src/main/java/com/example/slack/demo/service/slack/SlackBlockBuilderServiceImpl.java
+++ src/main/java/com/example/slack/demo/service/slack/SlackBlockBuilderServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.slack.demo.service.slack;
 
-
 import com.example.slack.demo.configuration.slack.SlackMessage;
 import com.slack.api.model.block.ActionsBlock;
 import com.slack.api.model.block.LayoutBlock;
@@ -9,7 +8,7 @@ import com.slack.api.model.block.composition.BlockCompositions;
 import com.slack.api.model.block.element.BlockElements;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +22,9 @@ public class SlackBlockBuilderServiceImpl implements SlackBuilderService {
 
     private final SlackApp slackApp;
 
+    @Value("${application.settings.integration.slack.user-email}")
+    private String defaultUserEmail;
+
     public static final String IGNORE_BUTTON = "ignore-button";
     public static final String DANGER = "danger";
 
@@ -34,7 +36,7 @@ public class SlackBlockBuilderServiceImpl implements SlackBuilderService {
                            .build());
         slackMessages.add(SlackMessage.builder()
                                   .blocks(blocks)
-                                  .userEmail("aoliva@griddynamics.com")
+                                  .userEmail(defaultUserEmail)
                                   .build());
         slackApp.sendMessage(slackMessages);
         return ResponseEntity.ok("The message was sent to the bot channel!");
@@ -49,7 +51,7 @@ public class SlackBlockBuilderServiceImpl implements SlackBuilderService {
         this.addIgnoreButton(blocks);
         slackMessages.add(SlackMessage.builder()
                                   .blocks(blocks)
-                                  .userEmail("aoliva@griddynamics.com")
+                                  .userEmail(defaultUserEmail)
                                   .build());
         slackApp.sendMessage(slackMessages);
         return ResponseEntity.ok("The message was sent to the bot channel!");
